4e17ba3f07460d439308cdff37763491be83d4ef
Daniel Imfeld <daniel@imfeld.dev>
2023-11-05 15:14:00 -1000

    start on interface

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,53 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use tauri::State;
+
+#[tauri::command]
+fn get_manifest(manifest: State<Manifest>) -> Manifest {
+    manifest.inner().to_owned()
+}
+
+#[tauri::command]
+fn read_audio_file(file: String) -> Result<Vec<u8>, String> {
+    let data = std::fs::read(file).map_err(|e| e.to_string())?;
+    Ok(data)
+}
+
+#[derive(Parser, Debug)]
+pub struct Cli {
+    file: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct AudioFile {
+    #[serde(default)]
+    name: String,
+    #[serde(default)]
+    directions: String,
+    path: String,
+    #[serde(default)]
+    length: u32,
+    volume: Option<f32>,
+    start_at: Option<f32>,
+    end_at: Option<f32>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Manifest {
+    items: Vec<AudioFile>,
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let args = Cli::parse();
+    let manifest_data = std::fs::read_to_string(args.file).expect("opening manifest file");
+    let manifest: Manifest = toml::from_str(&manifest_data).expect("decoding manifest");
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_manifest, read_audio_file])
+        .manage(manifest)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,134 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+  import * as Card from '$lib/components/ui/card';
+  import { Progress } from '$lib/components/ui/progress';
+  import { invoke, convertFileSrc } from '@tauri-apps/api/tauri';
+  import { onMount, tick } from 'svelte';
+
+  interface Manifest {
+    files: AudioFile[];
+  }
+
+  interface AudioFile {
+    name: string;
+    directions: string;
+    path: string;
+    startAt?: number;
+    stopAt?: number;
+    volume?: number;
+    length: number;
+  }
+
+  let manifest: Manifest | null;
+  onMount(async () => {
+    manifest = await invoke('get_manifest');
+  });
+
+  let currentIndex = 0;
+  $: currentFile = manifest?.files[currentIndex];
+
+  let audioEl: HTMLAudioElement;
+
+  async function playAudio(from = 0) {
+    if (!audioEl || !currentFile) {
+      return;
+    }
+
+    audioEl.src = convertFileSrc(currentFile.path);
+    let startAt = (currentFile.startAt ?? 0) + from;
+    audioEl.currentTime = startAt;
+    audioEl.volume = currentFile.volume ?? 1;
+    audioEl.play();
+  }
+
+  function finishedPlaying() {
+    if (!manifest) {
+      return;
+    }
+
+    if (currentIndex < manifest.files.length - 1) {
+      currentIndex++;
+      audioPos = 0;
+    }
+  }
+
+  function stop() {
+    audioEl.pause();
+    audioPos = 0;
+  }
+
+  function resume() {
+    audioEl.play();
+  }
+
+  function pause() {
+    audioEl.pause();
+  }
+
+  let audioPos = 0;
+  let paused = true;
+  let audioDuration = 0;
+</script>
+
+<audio
+  bind:this={audioEl}
+  bind:duration={audioDuration}
+  bind:currentTime={audioPos}
+  bind:paused
+  controls
+></audio>
+
+<div class="flex gap-2">
+  <Button variant="default" on:click={() => playAudio()}>Play</Button>
+  <Button
+    variant="default"
+    on:click={() => {
+      if (paused) {
+        resume();
+      } else {
+        pause();
+      }
+    }}
+    >{#if paused}Resume{:else}Pause{/if}</Button
+  >
+  <Button variant="default" on:click={stop}>Stop</Button>
+</div>
+<Progress value={audioPos} max={audioDuration} class="w-full" />
+
+{#each manifest?.files ?? [] as file, i}
+  <Card.Card>
+    <Card.CardHeader>
+      <Card.CardTitle>{file.name}</Card.CardTitle>
+    </Card.CardHeader>
+    <Card.CardContent>
+      {#if file.directions}
+        <p>{file.directions}</p>
+      {/if}
+      <div class="flex gap-2">
+        <Button
+          variant="default"
+          on:click={() => {
+            currentIndex = i;
+            tick().then(() => playAudio());
+          }}>Play</Button
+        >
+        {#if currentIndex == i}
+          <Button
+            variant="default"
+            on:click={() => {
+              if (paused) {
+                resume();
+              } else {
+                pause();
+              }
+            }}
+            >{#if paused}Resume{:else}Pause{/if}</Button
+          >
+          <Button variant="default" on:click={stop}>Stop</Button>
+        {/if}
+      </div>
+      <Progress value={i == currentIndex ? audioPos : 0} max={file.length} class="w-full" />
+    </Card.CardContent>
+  </Card.Card>
+  {file.name}
+{/each}
